632131d75f5e05b20e74b2e24e53bb8a474ea241
georso <hentrygeorge3041@gmail.com>
2022-01-08 16:49:02 +0530

    check

---
+++ src/org/get/GetMethod.java
@@ -0,0 +1,22 @@
+package org.get;
+
+
+public class GetMethod {
+	
+	private void apple () {
+		System.out.println("Apple is no 1 ");
+	}
+	private void iPhone() {
+		System.out.println("Iphone is no 2");
+	}
+	private void nokia() {
+		System.out.println("Nokia is no 3");
+	}
+	public static void main (String[] args) {
+		GetMethod g = new GetMethod();
+		g.apple();
+		g.iPhone();
+		g.nokia();
+	}
+
+}
